a5c6e8342f08a198579ac03927ffd1a105e4ed3e
Kirill Titov <work@kirilltitov.com>
2019-06-30 01:20:26 +0300

    DateFormatter broken in Linux

--- Sources/SwiftTestPackage/main.swift
+++ Sources/SwiftTestPackage/main.swift
@@ -1,20 +1,8 @@
 import Foundation
 
-let queue = DispatchQueue(label: "Test", qos: .userInitiated, attributes: .concurrent)
-let group = DispatchGroup()
+let formatter = DateFormatter()
+formatter.dateStyle = .long
+formatter.timeStyle = .short
+formatter.locale = Locale(identifier: "ru_RU")
 
-let started = Date().timeIntervalSince1970
-
-print("Hello, started at \(started)")
-
-queue.async(group: group, flags: .barrier) {
-    let entered = Date().timeIntervalSince1970
-    print("Entered async block 1 at \(entered) (𝚫 \(entered - started))")
-}
-
-queue.async(group: group, flags: .barrier) {
-    let entered = Date().timeIntervalSince1970
-    print("Entered async block 2 at \(entered) (𝚫 \(entered - started))")
-}
-
-group.wait()
+print(formatter.string(from: Date(timeIntervalSince1970: 1561846736.749614)))
